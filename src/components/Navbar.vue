d3db41f8b4e5fbd9563f6cf8e915618ef7d0273b
Abel Estevez <47267645+Abelito8991@users.noreply.github.com>
2021-10-27 04:10:11 -0400

    Navbar deshecho

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,39 +6,27 @@
                     <img class="btn p-0" @click="goInicio" :src="logosvg" alt="Logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navegationBar">
-                <!--Boton de contacto para movil-->
-                <button class="p-2 mt-4 mb-3 d-block d-lg-none d-sm-block contact_button d-flex justify-content-center">
-                    <a href="https://api.whatsapp.com/send?phone=5354986767&text=text=Quiero solicitar los servicios de CubanWebService.">
-                        <span>
-                            <img v-if="telfico !== null" :src="telfico">
-                        </span>
-                        WhatsApp  
-                        <!-- +53 5 498 67 67 -->
-                    </a>
+                
+
+                    <button @click="navHide" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navegationBar">
+
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                 </button>
-
-                <button @click="navHide" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navegationBar">
-
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
                 <!--Menu Colapsado-->
 
-                <div class="collapse navbar-collapse d-lg-flex bd-highlight" id="navegationBar">
-                    
+                <div class="collapse navbar-collapse d-md-flex bd-highlight" id="navegationBar">
                     <div class="navbar-nav mr-auto ms-auto bd-highlight">
-
-                <div class="navbar-collapse justify-content-md-center" :class="(navcollap)?'collapse':''" id="navegationBar">
-                    <div class="navbar-nav mr-auto">
-                        <router-link v-for="ruta in routes" :key="ruta.name" :to="ruta"
-                        class="nav-item nav-link mt-4 mx-5 text-decoration-none"
-                        v-slot="{isActive}" @click="navHide"
-                        >
-                        <a class="text-decoration-none fw-bold" :class="isActive? 'active':'normal-link'">{{ruta.name}}</a>
-                        </router-link>
-                    </div>
-
-                    <!--Boton de contacto-->
+                        <div class="navbar-collapse justify-content-md-center" :class="(navcollap)?'collapse':''" id="navegationBar">
+                            <div class="navbar-nav mr-auto">
+                                <router-link v-for="ruta in routes" :key="ruta.name" :to="ruta"
+                                class="nav-item nav-link mt-4 mx-5 text-decoration-none"
+                                v-slot="{isActive}" @click="navHide"
+                                >
+                                <a class="text-decoration-none fw-bold" :class="isActive? 'active':'normal-link'">{{ruta.name}}</a>
+                                </router-link>
+                            </div>
+                            <!--Boton de contacto-->
                     <div class="ms-auto bd-highlight">
                         <button class="p-2  mt-4 mb-3 contact_button">
                             <a href="https://api.whatsapp.com/send?phone=5354986767&Quiero solicitar los servicios de CubanWebService.">
@@ -50,10 +38,12 @@
                             </a>
                         </button>  
                     </div>
-                </div>   
+                        </div>   
 
                 
-            </div>   
+                    </div>   
+                </div>
+            </div>
     </nav>
 </template>
 
